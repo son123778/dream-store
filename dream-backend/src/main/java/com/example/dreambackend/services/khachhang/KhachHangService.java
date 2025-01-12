7cb020cb636e1757d4da2c3bc3bc01d0348862ea
duonghdph48079 <duonghdph48079@fpt.edu.vn>
2025-01-12 15:21:20 +0700

    test

---
+++ dream-backend/src/main/java/com/example/dreambackend/services/khachhang/KhachHangService.java
@@ -0,0 +1,57 @@
+package com.example.dreambackend.services.khachhang;
+
+import com.example.dreambackend.dtos.KhachHangDto;
+import com.example.dreambackend.entities.KhachHang;
+import com.example.dreambackend.repositories.KhachHangRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class KhachHangService implements IKhachHangService{
+    @Autowired
+    private KhachHangRepository khachHangRepository;
+
+    @Override
+    public List<KhachHang> getAllKhachHang() {
+        return khachHangRepository.findAll();
+    }
+
+    @Override
+    public KhachHang getKhachHangById(Integer id) {
+        return khachHangRepository.findById(id).get();
+    }
+
+    @Override
+    public KhachHang newKhachHang(KhachHangDto khachHangDto) {
+        KhachHang newKhachHang = KhachHang.builder()
+                .ma(khachHangDto.getMa())
+                .ten(khachHangDto.getTen())
+                .gioiTinh(khachHangDto.isGioiTinh())
+                .soDienThoai(khachHangDto.getSoDienThoai())
+                .matKhau(khachHangDto.getMatKhau())
+                .ngayTao(LocalDate.now())
+                .trangThai(khachHangDto.getTrangThai())
+                .build();
+                return  khachHangRepository.save(newKhachHang);
+
+
+    }
+
+    @Override
+    public KhachHang updateKhachHang(Integer id,KhachHangDto updateKhachHangDto) {
+        KhachHang updatedkh = getKhachHangById(id);
+        updatedkh.setTen(updateKhachHangDto.getTen());
+        updatedkh.setGioiTinh(updateKhachHangDto.isGioiTinh());
+        updatedkh.setMatKhau(updateKhachHangDto.getMatKhau());
+        updatedkh.setSoDienThoai(updateKhachHangDto.getSoDienThoai());
+        updatedkh.setNgaySua(LocalDate.now());
+        updatedkh.setTrangThai(updateKhachHangDto.getTrangThai());
+        khachHangRepository.save(updatedkh);
+        return updatedkh;
+    }
+
+
+}
