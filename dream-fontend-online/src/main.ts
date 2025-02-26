f93c1ab0e23fa2069e43bac6cb9c9442940d369d
Hoàng Huy Hùng <hoanghuyhung080704@gmail.com>
2025-02-26 20:17:10 +0700

    delete temp

--- dream-fontend-online/src/main.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {appConfig} from './app/app.config';
-import {AppComponent} from './app/app.component';
-
-// Khởi chạy ứng dụng với cấu hình `appConfig`.
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
