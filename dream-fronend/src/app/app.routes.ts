8628a1942e4773244e22d971a01307c0b546ddb7
Nguyễn Trường Sơn <150453888+sonntph48138@users.noreply.github.com>
2025-03-10 01:28:04 +0700

    update

--- dream-fronend/src/app/app.routes.ts
+++ dream-fronend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { BanhangComponent } from './banhang/banhang.component';
+import { HoaDonComponent } from './hoadon/hoadon.component';
 import { SanphamComponent } from './sanpham/sanpham.component';
 import { LayoutComponent } from './layout/layout.component';
 import { KhachhangComponent } from './khachhang/khachhang.component';
@@ -7,7 +8,7 @@ import { KhuyenmaiComponent } from './khuyenmai/khuyenmai.component';
 import { NhanvienComponent } from './nhanvien/nhanvien.component';
 import { ThongkeComponent } from './thongke/thongke.component';
 import { VoucherComponent } from './voucher/voucher.component';
-import { DangnhapComponent } from './dangnhap/dangnhap.component'; 
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
 // Các routes được cấu hình cho ứng dụng
 export const routes: Routes = [
   { path: '', component: DangnhapComponent }, // Trang đăng nhập là mặc định
@@ -17,6 +18,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'banhang', pathMatch: 'full' }, // Route mặc định
       { path: 'banhang', component: BanhangComponent }, // Bán hàng
+      { path: 'hoadon', component: HoaDonComponent }, // Hóa đơn
       { path: 'sanpham', component: SanphamComponent }, // Sản phẩm
       { path: 'voucher', component: VoucherComponent }, // Voucher
       { path: 'khuyenmai', component: KhuyenmaiComponent }, // Khuyến mãi
